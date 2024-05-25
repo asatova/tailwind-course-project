215214a0d52b953dd7162d749f14fce642c6afef
zarnigor <zarnigorasatova52@gmail.com>
2024-05-25 17:14:35 +0500

    feature

---
+++ tailwind-project/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className='border border-amber-700 rounded-lg'>
+        <h1 className='text-red-900'>Hello</h1>
+        <button className='mx-10'>Login</button>
+      </div>
+  )
+}
+export default App
